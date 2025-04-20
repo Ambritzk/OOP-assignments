af94ce9773aee0cb88e3c5f100d2bfe9e6d9f8cf
Ambritzk <143338955+Ambritzk@users.noreply.github.com>
2025-04-20 16:00:04 +0500

    Update assignment4.cpp

--- assignment4.cpp
+++ assignment4.cpp
@@ -22,36 +22,88 @@ public:
 		number = new int*[4];
 		string list[4] = {"mobile","work","home","other"};
 		for (int i = 0; i < 4; i++) {
-			cout << "How many digits do you need for " << list[i] << " number: ";
+			cout << "how many digits do you need for " << list[i] << " number: ";
 			cin >> size[i];
 
 			cout << size[i];
 			number[i] = new int[size[i]];
-			cout << "Enter number: ";
+			cout << "enter number: ";
 			for (int j = 0; j < size[i]; j++) {
 				cin >> number[i][j];
 			}
 			
 		}
 		char choice = ' ';
-		cout << "Do you want to add an email address? (y/n): ";
+		cout << "do you want to add an email address? (y/n): ";
 		cin >> choice;
 		if (choice == 'y') {
-			cout << "Enter email address: ";
+			cout << "enter email address: ";
 			cin >> email;
 		}
 
 
-		cout << "Do you want to add a postal address? (y/n): ";
+		cout << "do you want to add a postal address? (y/n): ";
 		cin >> choice;
 		if (choice == 'y') {
-			cout << "Enter postal address: ";
+			cout << "enter postal address: ";
 			cin >> postal_address;
 		}
 
 
-		cout << endl << "User " << name << " has  been created!" << endl;
+		cout << endl << "user " << name << " has  been created!" << endl;
 
+	}
+
+
+	bool AreEqual(int a[], int elements) {
+		if (name.empty()) {
+			return false;
+		}
+		for (int i = 0; i < 4; i++) {
+			if (elements != size[i]) {
+				continue;
+			}
+			bool same = true;
+			for (int j = 0; j < size[j]; j++) {
+				if (a[j] != number[i][j]) {
+					same = false;
+					break;
+				}
+			}
+
+			if (same) {
+				return same;
+			}
+		}
+		return false;
+	}
+
+	string getName() {
+		return name;
+	}
+	bool operator==(string& a) {
+		if (a.empty()) {
+			return false;
+		}
+		string first;
+		string last;
+		bool frst = true;
+		for (int i = 0; frst; i++) {
+
+			if (a[i] == '*' && i < a.length()) {
+				first = a.substr(0, i);
+				last = a.substr(i + 1, (a.length() - first.length() - 1));
+				frst = false;
+			}																				 
+
+		}
+		if (name.length() < first.length() + last.length()) {
+						return false;
+					
+		}
+		bool conditionOne = name.substr(0, first.length()) == first;		// ni*er
+		bool conditionTwo = name.substr(name.length() - last.length()) == last;
+		return (conditionOne && conditionTwo);
 	}
 
 	contact (contact& b) noexcept{ //cpy
@@ -150,7 +202,7 @@ public:
 		b.email = "";
 
 		postal_address = b.postal_address;
-		postal_address = "";
+		b.postal_address = "";
 
 		return *this;
 	}
@@ -204,20 +256,27 @@ public:
 		return same;
 	}
 
+
 	void print() {
 		if (!(this->name.empty())) {
-			cout << endl << name << ":\nNumbers:\n";
+			cout << endl << name << ":\nnumbers:\n";
 			string list[4] = { "mobile","work","home","other" };
 			for (int i = 0; i < 4; i++) {
-				cout << list[i] << ": ";
+				if (number[i] == nullptr) {
+					continue;
+				}
+				cout << "\t" << list[i] << ": ";
 				for (int j = 0; j < size[i]; j++) {
 					cout << number[i][j];
 				}
 				cout << endl;
 			}
 
-			if (!email.empty())cout << endl << "Email address: " << email;
-			if (!postal_address.empty())cout << "\nPostal address: " << postal_address << endl;
+			if (!email.empty())cout << endl << "email address: " << email;
+			if (!postal_address.empty())cout << "\npostal address: " << postal_address << endl;
+		}
+		else {
+			cout << "empty\n";
 		}
 	}
 
@@ -225,7 +284,10 @@ public:
 	~contact() {
 		if (!name.empty()) {
 			for (int i = 0; i < 4; i++) {
-				delete[] number[i];
+				if (number[i] != nullptr) {
+					delete[] number[i];
+				}
+
 			}
 			delete[] number;
 
@@ -242,8 +304,8 @@ private:
 public:
 	phonebook() {
 		list = new contact[10];
-		capacity = 5;
-		contacts = 5;
+		capacity = 10;
+		contacts = 4;
 	}
 
 	phonebook(phonebook& b) { //cpy
@@ -282,6 +344,10 @@ public:
 	}
 
 	phonebook operator=(phonebook&& b) noexcept{ //move
+		if (this == &b) {
+			return *this;
+		}
+
 		capacity = b.capacity;
 		b.capacity = 0;
 
@@ -295,7 +361,7 @@ public:
 		return *this;
 	}
 
-	void checksize() {
+	void expand() {
 		if (contacts >= capacity) {
 			int new_capacity = capacity == 0 ? 1 : capacity * 2;
 			contact* new_list = new contact[new_capacity];
@@ -315,45 +381,96 @@ public:
 
 	}
 
+	void shrink() {
+		if (contacts < capacity / 2) {
+			int new_capacity = capacity / 2;
+			contact* new_list = new contact[new_capacity];
+
+			for (int i = 0; i < contacts && i < new_capacity; i++) {
+				new_list[i] = list[i];
+			}
+			delete[] list;
+			list = new contact[new_capacity];
+			for (int i = 0; i < contacts && i < new_capacity; i++) {
+				list[i] = new_list[i];
+			}
+			capacity = new_capacity;
+
+			delete[] new_list;
+		}
+	}
 
 	void operator+(contact& a) {
-		checksize();
+		expand();
 		list[contacts++] = a;
 
-		cout << endl << "Contact has been added";
+		cout << endl << "contact has been added";
 	}
 
 	void operator-(contact& a) {
 		bool deleted = false;
 		for (int i = 0; i < contacts; i++) {
 			if (list[i] == a) {
-				deleted = true;
-			}
-
-			if (deleted && i < contacts - 1) {
-				list[i] = list[i + 1];
+				for (int j = i; j < contacts - 1; j++) {
+					list[j] = list[j + 1];
+				}
+				contacts--;
+				shrink();
+				cout << "\ncontact has been removed";
+				return;
 			}
 		}
-		if (deleted) {
-			contacts--;
-		}
-
 		cout << endl << "contact has been removed";
 	}
+
+	
 	~phonebook() {
 		delete[] list;
 	}
+	
 
 	void get() {
-		cout << endl << "Contacts: " << contacts << endl;
-		cout << "Capacity: " << capacity << endl;
+		cout << endl << "contacts: " << contacts << endl;
+		cout << "capacity: " << capacity << endl;
+		cout << endl << "------------------------";
+		
+		cout << "Contacts:\n";
 
 		for (int i = 0; i < contacts; i++) {
-			cout << "Contact" << i + 1 << ":\n";
-			list[i].print();
+			cout << i + 1 << ": ";
+			cout << list[i].getName();
 			cout << endl;
 		}
+		int choice = 0;
+		cout << "Enter the contact that you want to view information for:";
+		cin >> choice;
 
+		if (choice > 0 && choice <= contacts) {
+			list[choice + 1].print();
+		}
+
+
+	}
+
+	void searchbycontactname(string nem) {
+		for (int i = contacts - 1; i >= 0; i--) {
+			if (!(nem.empty())) {
+				if (list[i] == nem) {
+					cout << "Contact found:\n";
+					list[i].print();
+				}
+			}
+		}
+	}
+
+	void searchbycontactNumber(int a[], int size) {
+		for (int i = contacts - 1; i >= 0; i--) {
+
+			if (list[i].AreEqual(a,size)) {
+				cout << "Contact found:\n";
+				list[i].print();
+			}
+		}
 	}
 
 
@@ -362,20 +479,20 @@ public:
 
 int main() {
 	contact c1("abdul");
-	contact cecil("cecil");
-	c1.print();
+//	contact cecil("cecil");
 
 	phonebook sig;
-
-	sig.get();
 
 	sig + c1;
 
 	sig.get();
 
-	sig + cecil;
+	int a[] = { 1,2,3,4,5 };
+	sig.searchbycontactNumber(a, 5);
 
-	sig.get();
+	cout << "Now searching name:\n";
+	sig.searchbycontactname("a*l");
+
 
 
 }
